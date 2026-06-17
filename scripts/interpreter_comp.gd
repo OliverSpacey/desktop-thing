077bf154217ec7d9c57690401232780fb7cff51a
Oliver Spacey <Oliver.Spacey@gmail.com>
2026-06-17 11:12:07 +0100

    Added Interpreter Component
    Interpreter component added to handle the inputs for the game - will be added and removed from scenes as needed to allow/disable inpouts from the user and alter how they are interpreted. Just using this for now, can easily move to another scrfipt/node if needed

---
+++ scripts/interpreter_comp.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name InterpreterComponent
+
+#This components reads all inputs and interprets the actions to take based on context from the current scene and layout
+#Component chosen so it can be added/subtracted to/from scenes to mimic device locking, incorrect input mappings, etc. more easily
