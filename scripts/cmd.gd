e78542ffb84b5ee2937467b1fc49f008270bc4fb
Oliver Spacey <Oliver.Spacey@gmail.com>
2026-06-17 11:38:14 +0100

    Made cmd extend App, started logic for cmd terminal behaving like a real cmd terminal

--- scripts/cmd.gd
+++ scripts/cmd.gd
@@ -1,27 +1,19 @@
-extends Window
+extends App
 class_name cmd
 
-@export var app_icon_texture : Texture2D
-@export var app_default_width : int = 500
-@export var app_default_height : int = 500
 @export var cmd_entry_field : TextEdit
 
-var app_name = "cmd"
-var app_icon : AppIcon
 
 func _ready():
-	close_requested.connect(close_window)
+	app_name = "cmd"
+	focus_exited.emit()
 
-func close_window():
-	app_icon.parent_app = null
-	queue_free()
-
-func minimise_window():
-	visible = false
-
-func maximise_window():
-	visible = true
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("enter"):
+	if event.is_action_pressed("enter") and has_focus():
+		print("Check if cmd is focused")
 		print(cmd_entry_field.text)
+		print("Save text to new label and spawn it ABOVE the text entry field to push this down")
+		print("Clear text field")
+		cmd_entry_field.clear()
+		cmd_entry_field.clear()
--- scripts/desktop_test.gd
+++ scripts/desktop_test.gd
@@ -3,12 +3,35 @@ class_name Desktop
 
 @export var desktop_ui : DesktopUI
 @export var test_app : App
+@export var apps_on_start : Array[App] = []
 
 @onready var task_bar : TaskBar = desktop_ui.task_bar
-
-func _ready():
-	task_bar.add_app_to_task_bar(test_app)
 
 var apps : Array[App]
 var open_apps : Array[App]
 var minimised_apps : Array[App]
+
+
+func _ready():
+	#Finds any children in the scene that are apps (makes it easier to drop in test apps for now)
+	var TEMP_apps_to_add = TEMP_find_apps_as_children()
+	
+	#Adds any apps that need to load on start to array
+	add_apps_on_start(TEMP_apps_to_add)
+	
+	#Load apps on start
+	for app in apps_on_start:
+		task_bar.add_app_to_task_bar(app)
+	apps.append_array(apps_on_start)
+
+func add_apps_on_start(apps_to_add : Array[App]):
+	apps_on_start.append_array(apps_to_add)
+
+func TEMP_find_apps_as_children():
+	var found_apps : Array[App] = []
+	var children = get_children()
+	
+	for child in children:
+		if child is App:
+			found_apps.append(child)
+	return found_apps
