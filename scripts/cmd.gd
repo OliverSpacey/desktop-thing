3e171604f9fa529e672095ae6f301b865bc10935
Oliver Spacey <Oliver.Spacey@gmail.com>
2026-06-17 16:42:18 +0100

    Reworked cmd - now has running history of commands

--- scripts/cmd.gd
+++ scripts/cmd.gd
@@ -1,19 +1,14 @@
 extends App
 class_name cmd
 
-@export var cmd_entry_field : TextEdit
-
+@export var cmd_entry_field : LineEdit
+@export var text_history_label : Label
 
 func _ready():
 	app_name = "cmd"
-	focus_exited.emit()
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("enter") and has_focus():
-		print("Check if cmd is focused")
-		print(cmd_entry_field.text)
-		print("Save text to new label and spawn it ABOVE the text entry field to push this down")
-		print("Clear text field")
-		cmd_entry_field.clear()
+		text_history_label.text = text_history_label.text + "\n" + ">" + cmd_entry_field.text
 		cmd_entry_field.clear()
